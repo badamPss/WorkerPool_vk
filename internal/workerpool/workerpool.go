533f027e7632541b9cb5449506d378b2cace328a
badamPss <ignatvaschilo@yandex.ru>
2025-06-09 02:02:48 +0300

    add mutex

--- internal/workerpool/workerpool.go
+++ internal/workerpool/workerpool.go
@@ -1,12 +1,16 @@
 package workerpool
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type WorkerPool struct {
 	jobs    chan string
 	results chan<- string
 	workers map[int]*Worker
 	nextID  int
+	mu      sync.Mutex
 }
 
 func NewWorkerPool(jobs chan string, result chan<- string) *WorkerPool {
@@ -17,19 +21,23 @@ func NewWorkerPool(jobs chan string, result chan<- string) *WorkerPool {
 }
 
 func (wp *WorkerPool) Add() {
+	wp.mu.Lock()
 	worker := NewWorker(wp.nextID, wp.jobs, wp.results)
 	wp.workers[wp.nextID] = worker
 	fmt.Printf("Worker %d added\n", wp.nextID)
-	worker.Run()
 	wp.nextID++
+	wp.mu.Unlock()
+	worker.Run()
 }
 
 func (wp *WorkerPool) Remove(id int) {
+	wp.mu.Lock()
 	if worker, ok := wp.workers[id]; ok {
 		worker.Stop()
 		delete(wp.workers, id)
 		fmt.Printf("Worker %d removed\n", id)
 	}
+	wp.mu.Unlock()
 }
 
 func (wp *WorkerPool) Stop() {
